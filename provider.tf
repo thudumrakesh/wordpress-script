7497c30eb95854b00019125ea7945aec34c4cf90
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-02-21 16:32:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = AKIA46HSNATSJGDNX35R
-  secret_key = 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
+  access_key = "AKIA46HSNATSJGDNX35R"
+  secret_key = "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"
 }
