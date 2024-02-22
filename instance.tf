91fb49565054eac4541fe76f40d8f3fcd89b5f81
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-02-22 23:10:31 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "WORDPRESS" {
   ami           = "ami-0cf7b2f456cd5efd4"
   instance_type = "t2.micro"
 
-  key_name                    = "TTT"
+  key_name                    = "new"
   vpc_security_group_ids      = [aws_security_group.sg1.id]
   subnet_id                   = aws_subnet.publicsubnet1.id
   associate_public_ip_address = true
